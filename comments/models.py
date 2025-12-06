de42d09a90f9939b595227f01f5dd54eea678d94
Profy8712 <profysss8712@gmail.com>
2025-12-06 16:44:52 +0200

    Add Comment and Attachment models

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    homepage = models.URLField(blank=True, null=True)
+    text = models.TextField()
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user_name}: {self.text[:30]}'
+
+
+class Attachment(models.Model):
+    comment = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+        related_name='attachments'
+    )
+    file = models.FileField(upload_to='attachments/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
