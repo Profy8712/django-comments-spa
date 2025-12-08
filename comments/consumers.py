bb58bf4b45f5b098f92e6b76531253e2d61e851f
Profy8712 <profysss8712@gmail.com>
2025-12-09 00:55:34 +0200

    Fix async comment notifications with Channels

--- comments/consumers.py
+++ comments/consumers.py
@@ -1,34 +1,52 @@
-import json
-
+# comments/consumers.py
 from asgiref.sync import sync_to_async
-from channels.generic.websocket import AsyncWebsocketConsumer
-from rest_framework.renderers import JSONRenderer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 from .models import Comment
 from .serializers import CommentSerializer
 
 
-class CommentsConsumer(AsyncWebsocketConsumer):
+class CommentsConsumer(AsyncJsonWebsocketConsumer):
     group_name = "comments"
 
     async def connect(self):
-        """Handle WebSocket connection."""
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
-    async def disconnect(self, close_code):
-        """Handle WebSocket disconnection."""
+    async def disconnect(self, code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    async def receive(self, text_data=None, bytes_data=None):
-        """Client messages are not used in this simple example."""
+    async def receive_json(self, content, **kwargs):
+        """
+        We do not expect messages from client right now,
+        so this can stay empty or be used for debug.
+        """
         return
 
     async def comment_created(self, event):
-        """Handle 'comment_created' event from backend."""
+        """
+        Called from CommentListCreateView.perform_create via channel_layer.group_send.
+        event = {"type": "comment_created", "comment_id": <int>}
+        """
         comment_id = event.get("comment_id")
-        comment = await sync_to_async(Comment.objects.get)(pk=comment_id)
-        serializer = CommentSerializer(comment)
-        data = serializer.data
+        if comment_id is None:
+            return
 
-        await self.send(text_data=json.dumps({"type": "comment_created", "comment": data}))
+        # All ORM and serializer work must run in a sync thread
+        data = await self._get_serialized_comment(comment_id)
+
+        await self.send_json(
+            {
+                "type": "comment_created",
+                "payload": data,
+            }
+        )
+
+    @sync_to_async
+    def _get_serialized_comment(self, comment_id: int) -> dict:
+        comment = (
+            Comment.objects.select_related("parent")
+            .prefetch_related("attachments")
+            .get(pk=comment_id)
+        )
+        return CommentSerializer(comment).data
